749b3a63058312febbbc417384dfc3adc236b9aa
cy <cy@promote.cache-dns.local>
2016-08-28 10:39:50 +0800

    add dev-log file

--- jigsaw-puzzle/jigsaw-puzzle/ViewController.swift
+++ jigsaw-puzzle/jigsaw-puzzle/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func drawLine(sender: UIButton) {
+        var ctx: CGContextRef = UIGraphicsGetCurrentContext()!
+        
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
