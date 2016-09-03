d25b28f12862d114622c5e0153e927cf092a8520
cy <cy@cydemac.local>
2016-09-03 12:49:31 +0800

    arrange code

--- jigsaw-puzzle/jigsaw-puzzle/DrawView.swift
+++ jigsaw-puzzle/jigsaw-puzzle/DrawView.swift
@@ -10,6 +10,8 @@ import UIKit
 import ImageIO
 
 class DrawView: UIView {
+    var ctx : CGContext? = nil
+    var cgImage : CGImage? = nil
 
     /*
     // Only override drawRect: if you perform custom drawing.
@@ -19,14 +21,18 @@ class DrawView: UIView {
     }
     */
     override func drawRect(rect: CGRect) {
-        let ctx = UIGraphicsGetCurrentContext()
+        if ctx == nil {
+            ctx = UIGraphicsGetCurrentContext()
+        }
         CGContextMoveToPoint(ctx, 0, 0)
         CGContextAddLineToPoint(ctx, 150, 200)
         CGContextStrokePath(ctx)
         
-        let imagePath = NSBundle.mainBundle().pathForResource("test99", ofType: "bmp")
-        var uiimg = UIImage.init(named:imagePath!, inBundle:nil, compatibleWithTraitCollection:nil)
-        var cgImage = uiimg!.CGImage;
+        if cgImage == nil {
+            let imagePath = NSBundle.mainBundle().pathForResource("test99", ofType: "bmp")
+            let uiimg = UIImage.init(named:imagePath!, inBundle:nil, compatibleWithTraitCollection:nil)
+            cgImage = uiimg!.CGImage;
+        }
         //var rect = CGRectMake(0, 0, 600, 600)
         //CGContextDrawImage(ctx, rect, cgImage)
         
@@ -37,7 +43,7 @@ class DrawView: UIView {
             for y in [0.0, 200.0, 400.0] {
                 srcRect.origin.x = CGFloat(x)
                 srcRect.origin.y = CGFloat(y)
-                var subImage = CGImageCreateWithImageInRect(cgImage, srcRect)
+                let subImage = CGImageCreateWithImageInRect(cgImage, srcRect) //TODO: release subImage
                 CGContextDrawImage(ctx, dstRect, subImage)
                 dstRect.origin.y = dstRect.origin.y + dstRect.size.height + 20
             }
@@ -46,17 +52,18 @@ class DrawView: UIView {
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-        //print("touch begin.\(event!)");
         let p = (touches as NSSet).anyObject()?.locationInView(self)
         print("touch begin.\(p!.x), \(p!.y)")
     }
     
     override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
-        print("touch end.");
+        let p = (touches as NSSet).anyObject()?.locationInView(self)
+        print("touch end.\(p!.x), \(p!.y)")
     }
     
     override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
-        print("touch moved.");
+        let p = (touches as NSSet).anyObject()?.locationInView(self)
+        print("touch moved.\(p!.x), \(p!.y)")
     }
     
 }
